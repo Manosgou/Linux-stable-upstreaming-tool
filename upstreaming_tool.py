78ab43cd5e38b9490f857c7bda2353464dcb4a34
Manosgou <manolisgouvrikos@gmail.com>
2019-10-16 00:44:25 +0300

    Minor update

--- upstreaming_tool.py
+++ upstreaming_tool.py
@@ -1,8 +1,8 @@
 import os
+import sys
 import requests
 import time as t
 from bs4 import BeautifulSoup
-
 
 
 def welcome():
@@ -12,70 +12,83 @@ def welcome():
     print("1. Check current version")
     print("2. Check for updates")
     print("3. Upstream!(updade)")
+    print("4. Exit")
     select = input("Select: ")
 
     return select
- 
 
 
 def check_current_version():
-    kernel_version =[]
-    with open('Makefile','r') as f:
-        for lines in f.readlines()[:3]:        
+    kernel_version = []
+    with open('Makefile', 'r') as f:
+        for lines in f.readlines()[:3]:
             for words in lines.split()[2:3]:
-                kernel_version.append(words) 
+                kernel_version.append(words)
     return kernel_version
-    
-    
+
 
 def latest_version():
     url = "https://www.kernel.org"
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
-    releases_table = soup.find(id ='releases')
+    releases_table = soup.find(id='releases')
     versions = releases_table.findAll('strong')
-    online_versions=[]
+    versions_table = []
     for i in versions:
-        online_versions.append(i.text)
-    
-    
-    return online_versions[3:8]
-                   
-            
+        versions_table.append(i.text)
 
-    
+    return versions_table[3:8]
+
+
 def check_for_updates():
-    vps =check_current_version()
-    print("Your current linux stable version is: ",vps)        
-    clv=vps[0]+"."+vps[1]
-    print
-    ov= latest_version()
-    matching = [s for s in ov if clv in s]
-    print("Latest linux stable version is:",matching)
-    
-    
-    
+    ccv = check_current_version()
+    current= ccv[0]+"."+ ccv[1]+"."+ ccv[2]
+    print("Your current linux stable version is: " + current)
+    vp = ccv[0]+"."+ ccv[1]
+    lv = latest_version()
+    matching = [s for s in lv if vp in s]       
+    if current in matching:
+        print("You are up-to-date")
+        t.sleep(0.5)
+        sys.exit()
+    else:
+
+        print("You need to update,latest linux stable is:", matching)
 
 
 def upstream():
-    print("Under construction.....")   
-    
-    
+    print("Do you want to upstream(update) your current linux-stable version?(y/n)")
+    answer = input()
+    if answer == 'y' or answer == 'Y':
+        print("Working on it.....")
+    elif answer == 'n' or answer == 'N':
+        main()
+
+    else:
+        print("Aborting...")
+        t.sleep(0.5)
+        main()
 
 
 def main():
-    w =welcome()
-    if w=='1':
-        ccv= check_current_version()
-        print("Your current linux stable version is: ",ccv)
+    w = welcome()
+    if w == '1':
+        ccv = check_current_version()
+        current= ccv[0]+"."+ ccv[1]+"."+ ccv[2] 
+        print("Your current linux stable version is: " + current)
         print("Further more:")
-        print("VERSION = ",ccv[0])
-        print("PATCHLEVEL = ",ccv[1])
-        print("SUBLEVEL = ",ccv[2])
-    elif w=='2':
+        print("VERSION = ", ccv[0])
+        print("PATCHLEVEL = ", ccv[1])
+        print("SUBLEVEL = ", ccv[2])
+    elif w == '2':
         check_for_updates()
-    elif w=='3':
+    elif w == '3':
         upstream()
+
+    elif w == '4':
+        print("Exiting...")
+        t.sleep(0.3)
+        sys.exit()
     else:
         print("You must select one of the options above")
         print("")
@@ -83,13 +96,7 @@ def main():
         print("")
         t.sleep(1)
         main()
+
+
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
